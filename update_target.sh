82d4e6f182b0fd76f98a41f743492a62bf46b0ec
Bernhard Walle <bernhard@bwalle.de>
2009-09-20 19:23:07 +0200

    Add script to update target and a version file

---
+++ update_target.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Copyright (c) 2009, Bernhard Walle <bernhard@bwalle.de>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY <copyright holder> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+TEMPDIR=`mktemp -d`
+REVISION=`hg id -i`
+VERSION=`cat VERSION`
+TARGET=$1
+
+cleanup()
+{
+    rm -fr $TEMPDIR
+}
+
+error()
+{
+    echo "$@"
+    cleanup
+    exit 1
+}
+
+usage()
+{
+    echo "$0 <targetdirectory>"
+    exit 1
+}
+
+
+if [ -z "$TARGET" ] ; then
+    usage
+    exit 0
+fi
+
+hg archive -I src/ $TEMPDIR/libbw
+
+if ! [ -d "$TARGET" ] ; then
+    echo -n "Target '$TARGET' does not exist. Should I create the directory [y/n]? "
+    read answer
+    if [ "$answer" = "y" ] || [ "$answer" = "j" ] ; then
+        mkdir -p "$TARGET"
+    else
+        error "Ok. So I'll abort!"
+    fi
+fi
+
+rsync -v -r --delete $TEMPDIR/libbw/src/* $TARGET
+
+echo > $TARGET/.libbw <<EOF
+HG_COMMIT=$REVISION
+VERSION=$VERSION
+EOF
+
+cleanup
+
+# vim: set sw=4 ts=4 et::
