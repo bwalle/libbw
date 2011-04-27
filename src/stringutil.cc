87ceb6910f11406cc691a818416e4a110b4423e6
Bernhard Walle <bernhard@bwalle.de>
2011-04-27 21:44:28 +0200

    Fix bug in replace_char when multiple replacements have to be made
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- src/stringutil.cc
+++ src/stringutil.cc
@@ -132,7 +132,7 @@ std::string replace_char(const std::string  &input,
     std::string ret = input;
     size_t pos = 0;
 
-    while ((pos = input.find(old_char, pos)) != std::string::npos) {
+    while ((pos = ret.find(old_char, pos)) != std::string::npos) {
         ret.erase(pos, 1);
         ret.insert(pos, new_string);
 
